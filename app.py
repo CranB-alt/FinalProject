81030ae80098b1bf569a70d0cf5076bbdc3b1847
Brian Cranston <anul1on86@gmail.com>
2026-01-29 21:19:26 -0800

    Updated HTML and app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import numpy as np
 
 app = Flask(__name__)
 
-#Load pkl model
+# Load pkl model
 with open("penguin_model.pkl", "rb") as f:
     model = pickle.load(f)
 
-#Labeling
+# Label mapping
 species_map = {
     0: "Adelie",
     1: "Chinstrap",
@@ -20,18 +20,18 @@ def index():
     prediction = None
 
     if request.method == "POST":
-        #Form inputs
+        # Form inputs
         bill_depth = float(request.form["bill_depth"])
         island = request.form["island"]
 
-        #One-hot encode island (default)
+        # One-hot encode island
         island_Dream = 1 if island == "Dream" else 0
         island_Torgersen = 1 if island == "Torgersen" else 0
 
-        #Feature vector in correct order
+        # Feature vector in correct order
         features = np.array([[bill_depth, island_Dream, island_Torgersen]])
 
-        #Prediction
+        # Prediction
         pred_label = model.predict(features)[0]
         prediction = species_map[pred_label]
 
